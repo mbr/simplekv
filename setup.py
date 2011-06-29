2031125364b904027abc1b8a720e2b453465f2fb
Marc Brinkmann <git@marcbrinkmann.de>
2011-06-29 10:23:38 +0200

    Bumped version to 0.2.
    The Mixin -> Decorator change brought API differences, therefore bumping
    version number.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
     test_suite = 'unittest.collector'
 
 setup(name='simplekv',
-      version='0.1',
+      version='0.2',
       description='A simple key-value storage for binary data.',
       author='Marc Brinkmann',
       author_email='git@marcbrinkmann.de',
