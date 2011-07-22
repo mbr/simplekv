6e19a29f71b306ec5956ccbe88d3a5ab4ac5e952
Marc Brinkmann <git@marcbrinkmann.de>
2011-07-22 15:59:46 +0200

    Added extra test for caching decorator.

--- test/test_cache.py
+++ test/test_cache.py
@@ -13,9 +13,21 @@ from simplekv.memory import DictStore
 from simplekv.cache import CacheDecorator
 
 
-class TestCachingDecorator(unittest.TestCase, SimpleKVTest):
+class TestCacheDecorator(unittest.TestCase, SimpleKVTest):
     def setUp(self):
         self.backing_store = DictStore()
         self.cache = DictStore()
 
         self.store = CacheDecorator(self.cache, self.backing_store)
+
+    def test_works_when_cache_loses_key(self):
+        self.store.put('keya', 'valuea')
+        self.store.put('keyb', 'valueb')
+
+        self.assertEqual('valuea', self.store.get('keya'))
+        self.assertEqual('valueb', self.store.get('keyb'))
+
+        del self.store.cache.d['keya']
+
+        self.assertEqual('valuea', self.store.get('keya'))
+        self.assertEqual('valueb', self.store.get('keyb'))
