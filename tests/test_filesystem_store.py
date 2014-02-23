30544e02b24ea38dea18708063dfd486b06aafb4
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-23 18:25:38 +0100

    Ported over id-gen tests.

--- tests/test_filesystem_store.py
+++ tests/test_filesystem_store.py
@@ -3,14 +3,14 @@
 from simplekv.fs import FilesystemStore
 from tempdir import TempDir
 
-
 from basic_store import BasicStore
 from url_store import UrlStore
+from idgens import UUIDGen, HashGen
 
 import pytest
 
 
-class TestFilesystemStore(BasicStore, UrlStore):
+class TestFilesystemStore(BasicStore, UrlStore, UUIDGen, HashGen):
     @pytest.yield_fixture
     def store(self):
         with TempDir() as tmpdir:
