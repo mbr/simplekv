e92fe099b18f6583b8ff31e2c75c8d6d49bf1a1f
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-23 20:25:08 +0100

    Tests for HMAC functionality.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,8 @@ import pytest
 @pytest.fixture(params=['sha1', 'sha256', 'md5'])
 def hashfunc(request):
     return getattr(hashlib, request.param)
+
+
+@pytest.fixture(params=['secret_key_a', '\x12\x00\x12test'])
+def secret_key(request):
+    return request.param
--- tests/test_dict_store.py
+++ tests/test_dict_store.py
@@ -4,11 +4,12 @@ from simplekv.memory import DictStore
 
 from basic_store import BasicStore
 from idgens import UUIDGen, HashGen
+from test_hmac import HMACDec
 
 import pytest
 
 
-class TestDictStore(BasicStore, UUIDGen, HashGen):
+class TestDictStore(BasicStore, UUIDGen, HashGen, HMACDec):
     @pytest.fixture
     def store(self):
         return DictStore()
