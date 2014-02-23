dc7e40ec6e4325266f8e67a0eb150ea8687327e0
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-23 22:29:35 +0100

    Ported memcache tests.

---
+++ tests/test_memcache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from simplekv.memory.memcachestore import MemcacheStore
+
+from basic_store import BasicStore
+
+import pytest
+
+memcache = pytest.importorskip('memcache')
+
+
+class TestMemcacheStore(BasicStore):
+    @pytest.yield_fixture()
+    def store(self):
+        mc = memcache.Client(['localhost:11211'])
+
+        mc.set('foo', 'bar')
+        if not mc.get('foo') == 'bar':
+            pytest.skip('memcache connection not working')
+        mc.flush_all()
+        yield MemcacheStore(mc)
+        mc.flush_all()
+
+    # disabled tests (not fully API support for memcache)
+    test_has_key = None
+    test_has_key_with_delete = None
+    test_key_iterator = None
+    test_keys = None
+
+    def test_keys_throws_io_error(self, store):
+        with pytest.raises(IOError):
+            self.store.keys()
+
+        with pytest.raises(IOError):
+            self.store.iter_keys()
+
+        with pytest.raises(IOError):
+            iter(self.store)
+
+    def test_contains_throws_io_error_or_succeeds(self, store):
+        try:
+            'a' in store
+        except IOError:
+            pass
