390981d6b00b3260cfec87e29062039c879ad804
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-24 00:46:54 +0100

    Added bucket manager.

---
+++ tests/test_bucket_manager.py
@@ -0,0 +1,48 @@
+import pytest
+
+boto = pytest.importorskip('boto')
+from boto.exception import StorageResponseError
+
+from bucket_manager import boto_credentials, boto_bucket
+
+
+@pytest.fixture(params=boto_credentials,
+                ids=[c['access_key'] for c in boto_credentials])
+def credentials(request):
+    return request.param
+
+
+@pytest.yield_fixture()
+def bucket(credentials):
+    with boto_bucket(**credentials) as bucket:
+        yield bucket
+
+
+def test_simple(bucket):
+    pass
+
+
+def test_simple_with_contents(bucket):
+    k = bucket.new_key('i_will_prevent_deletion')
+    k.set_contents_from_string('meh')
+
+
+def test_context_manager_deletes_bucket(credentials):
+    with boto_bucket(access_key=credentials['access_key'],
+                     secret_key=credentials['secret_key'],
+                     connect_func=credentials['connect_func']) as bucket:
+        k = bucket.new_key('test_key')
+        k.set_contents_from_string('asdf')
+        bucket_name = bucket.name
+
+    # check if the bucket was deleted
+    conn = getattr(boto, credentials['connect_func'])(
+        credentials['access_key'], credentials['secret_key']
+    )
+
+    try:
+        conn.get_bucket(bucket_name)
+    except StorageResponseError as e:
+        assert e.code == 'NoSuchBucket'
+    else:
+        assert False, 'bucket not deleted'
