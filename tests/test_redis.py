9b4fd035ee442d8f5e532184a397800beb344fff
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-23 21:46:10 +0100

    Better redis tests.

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -3,37 +3,23 @@
 from basic_store import BasicStore
 
 import pytest
+redis = pytest.importorskip('redis')
 
-
-@pytest.fixture(scope='module')
-def redis_mod():
-    try:
-        import redis
-    except ImportError:
-        pytest.skip('redis package is not installed.')
-
-    return redis
-
-
-@pytest.yield_fixture()
-def redis_con(redis_mod):
-    from redis.exceptions import ConnectionError
-
-    # no configuration, for now
-    con = redis_mod.StrictRedis()
-
-    try:
-        con.get('anything')
-    except ConnectionError:
-        pytest.skip('Could not connect to redis server')
-
-    con.flushdb()
-    yield con
-    con.flushdb()
+from redis import StrictRedis
+from redis.exceptions import ConnectionError
 
 
 class TestRedisStore(BasicStore):
     @pytest.fixture()
     def store(self, redis_con):
         from simplekv.memory.redisstore import RedisStore
-        return RedisStore(redis_con)
+        r = StrictRedis()
+
+        try:
+            r.get('anything')
+        except ConnectionError:
+            pytest.skip('Could not connect to redis server')
+
+        r.flushdb()
+        yield RedisStore()
+        r.flushdb()
