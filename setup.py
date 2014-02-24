ad69ed0c3e6b4438f45edc8f2668ccb3c2b29adc
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-24 01:51:48 +0100

    Cleaned up setup.py.

--- setup.py
+++ setup.py
@@ -2,13 +2,8 @@
 # coding=utf8
 
 import os
-import sys
 
 from setuptools import setup, find_packages
-
-# the "test" command has been removed, as the test discovery does not
-# work with the packaged unittest in python 2.7 (it's missing the
-# unittest2.collector)
 
 
 def read(fname):
@@ -17,23 +12,12 @@ def read(fname):
 
 setup(name='simplekv',
       version='0.7.1.dev2',
-      description='A key-value storage for binary data, support many '\
-                  'backends.',
+      description=('A key-value storage for binary data, support many '
+                   'backends.'),
       long_description=read('README.rst'),
-      keywords='key-value-store storage key-value db database s3 gs boto'\
-               'memcache cache',
       author='Marc Brinkmann',
       author_email='git@marcbrinkmann.de',
       url='http://github.com/mbr/simplekv',
       license='MIT',
       packages=find_packages(exclude=['test']),
-      py_modules=[],
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python',
-          'Topic :: Database',
-          'Topic :: Software Development :: Libraries',
-      ]
-     )
+      )
