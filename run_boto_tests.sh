922f54f330461fc93f1927f857e7b511100600a2
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-24 14:43:46 +0100

    100 parallel tests hits bucket limit too often.

--- run_boto_tests.sh
+++ run_boto_tests.sh
@@ -2,4 +2,4 @@
 
 # this will stop you from growing old before 1000 s3 tests finish
 # requires pytest-xdist
-py.test -f -n 100 --boxed tests/test_boto_store.py
+py.test -f -n 10 --boxed tests/test_boto_store.py
