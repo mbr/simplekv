fc4cd7ebe2b9deba10e90b6bda56f2a776759ece
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-24 01:57:41 +0100

    Made key/value pairs available through fixtures.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import hashlib
 
+from six import b
+
 import pytest
 
 
@@ -10,4 +12,15 @@ def hashfunc(request):
 
 @pytest.fixture(params=['secret_key_a', '\x12\x00\x12test'])
 def secret_key(request):
+    return request.param
+
+
+# values are short payloads to store
+@pytest.fixture(params=[b('a_short_value'), b('another_short_value')])
+def value(request):
+    return request.param
+
+
+@pytest.fixture(params=[b('short_key'), b('different_key')])
+def key(request):
     return request.param
