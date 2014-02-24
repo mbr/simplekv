70fd3dd9290e318b1a04aa575f354b838ae44357
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-24 01:37:20 +0100

    Less whitespace, please

--- simplekv/_compat.py
+++ simplekv/_compat.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """Helpers for python 2/3 compatibility"""
 
 import sys
@@ -11,23 +10,19 @@ if not PY2:
 else:
     import ConfigParser
 
-
 if not PY2:
     from urllib.parse import quote as url_quote
 else:
     from urllib import quote as url_quote
-
 
 if not PY2:
     from urllib.parse import urlparse
 else:
     from urlparse import urlparse
 
-
 if not PY2:
     imap = map
 else:
     from itertools import imap
-
 
 xrange = range if not PY2 else xrange
