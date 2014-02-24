623173ac9cd3eece7c67ea95b28f3d5a432c6346
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-24 02:40:13 +0100

    Do not rely on isinstance/basestring tests.

--- tests/url_store.py
+++ tests/url_store.py
@@ -6,7 +6,7 @@ import pytest
 class UrlStore(object):
     def test_url_for_for_generates_url_for(self, store, key, value):
         store.put(key, value)
-        assert isinstance(store.url_for(key), basestring)
+        assert store.url_for(key)
 
     def test_url_for_generation_does_not_check_exists(self, store, key):
         store.url_for(key)
