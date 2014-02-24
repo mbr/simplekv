ff336e34ab2996c0e01378945b10e4f3bc870a2e
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-24 01:36:09 +0100

    Use PY2 check instead of PY3 check.
    See http://lucumr.pocoo.org/2013/5/21/porting-to-python-3-redux/ for
    details.

--- simplekv/_compat.py
+++ simplekv/_compat.py
@@ -3,31 +3,31 @@
 
 import sys
 
-PY3 = sys.version_info[0] == 3
+PY2 = sys.version_info[0] == 2
 
 
-if PY3:
+if not PY2:
     import configparser as ConfigParser
 else:
     import ConfigParser
 
 
-if PY3:
+if not PY2:
     from urllib.parse import quote as url_quote
 else:
     from urllib import quote as url_quote
 
 
-if PY3:
+if not PY2:
     from urllib.parse import urlparse
 else:
     from urlparse import urlparse
 
 
-if PY3:
+if not PY2:
     imap = map
 else:
     from itertools import imap
 
 
-xrange = range if PY3 else xrange
+xrange = range if not PY2 else xrange
