bc8e7df0a43d85e2a3540349298330d4d24d1aee
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-26 03:25:34 +0100

    Release version 0.8.4.
    (commit by unleash 0.5.8.dev1)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 setup(name='simplekv',
-      version='0.8.4.dev1',
+      version='0.8.4',
       description=('A key-value storage for binary data, support many '
                    'backends.'),
       long_description=read('README.rst'),
