b78f285d7113c4fc457cf03c382cccbf14a365b2
Marc Brinkmann <git@marcbrinkmann.de>
2015-10-23 14:57:34 +0200

    Support subdirs in git module.

--- simplekv/git.py
+++ simplekv/git.py
@@ -1,10 +1,48 @@
 from io import BytesIO
+import re
 import time
 
 from dulwich.repo import Repo
 from dulwich.objects import Commit, Tree, Blob
 
 from . import KeyValueStore, __version__
+
+
+def on_tree(repo, tree, components, obj):
+    """Mounts an object on a tree, using the given path components.
+
+    :param tree: Tree object to mount on.
+    :param components: A list of strings of subpaths (i.e. ['foo', 'bar'] is
+                       equivalent to '/foo/bar')
+    :param obj: Object to mount.
+    :return: A list of new entities that need to be added to the object store,
+             where the last one is the new tree.
+    """
+
+    # pattern-matching:
+    if len(components) == 1:
+        if isinstance(obj, Blob):
+            mode = 0o100644
+        elif isinstance(obj, Tree):
+            mode = 0o040000
+        else:
+            raise TypeError('Can only mount Blobs or Trees')
+        name = components[0]
+        tree[name] = mode, obj.id
+        return [tree]
+    elif len(components) > 1:
+        a, bc = components[0], components[1:]
+        if a in tree:
+            a_tree = repo[tree[a][1]]
+            if not isinstance(a_tree, Tree):
+                a_tree = Tree()
+        else:
+            a_tree = Tree()
+        res = on_tree(repo, a_tree, bc, obj)
+        tree[a] = 0o040000, res[-1].id
+        return res + [tree]
+    else:
+        raise ValueError('Components can\'t be empty.')
 
 
 class GitCommitStore(KeyValueStore):
@@ -14,7 +52,10 @@ class GitCommitStore(KeyValueStore):
     def __init__(self, repo_path, branch=b'master', subdir=b''):
         self.repo = Repo(repo_path)
         self.branch = branch
-        self.subdir = subdir
+
+        # cleans up subdir, to a form of 'a/b/c' (no duplicate, leading or
+        # trailing slashes)
+        self.subdir = re.sub('#/+#', '/', subdir.strip('/'))
 
     @property
     def _refname(self):
@@ -82,7 +123,8 @@ class GitCommitStore(KeyValueStore):
 
     def _put_file(self, key, file):
         commit = self._create_top_commit()
-        commit.message = ('Updated key {!r}'.format(key)).encode('utf8')
+        commit.message = ('Updated key {!r}'.format(self.subdir + '/' + key)
+                          ).encode('utf8')
 
         # prepare blob
         if hasattr(file, 'read'):
@@ -101,17 +143,21 @@ class GitCommitStore(KeyValueStore):
             commit.parents = [parent_commit.id]
             tree = self.repo[parent_commit.tree]
 
-        subtree = tree
+        objects_to_add = [blob]
 
         # no subdirs in filename, add directly to tree
-        subtree.add(key.encode('ascii'), 0o100644, blob.id)
+        components = [key.encode('ascii')]
+        if self.subdir:
+            components = self.subdir.split('/') + components
+        res = on_tree(self.repo, tree, components, blob)
+        objects_to_add.extend(res)
 
-        commit.tree = tree.id
+        commit.tree = res[-1].id
+        objects_to_add.append(commit)
 
         # add objects
-        self.repo.object_store.add_object(blob)
-        self.repo.object_store.add_object(tree)
-        self.repo.object_store.add_object(commit)
+        for obj in objects_to_add:
+            self.repo.object_store.add_object(obj)
 
         # update refs
         self.repo.refs[self._refname] = commit.id
--- tests/test_git_store.py
+++ tests/test_git_store.py
@@ -6,12 +6,15 @@ import pytest
 
 from simplekv.git import GitCommitStore
 
-# FIXME: test subdir support
-
 
 class TestGitCommitStore(BasicStore, UUIDGen, HashGen):
     @pytest.fixture(params=['master', 'not-master', 'other-branch'])
     def branch(self, request):
+        return request.param
+
+    @pytest.fixture(params=['', '/', 'subdir', 'subdir/', 'sub/subdir/',
+                            '/sub/subdir', '/sub/subdir/'])
+    def subdir_name(self, request):
         return request.param
 
     @pytest.yield_fixture
@@ -21,5 +24,44 @@ class TestGitCommitStore(BasicStore, UUIDGen, HashGen):
             yield tmpdir
 
     @pytest.fixture
-    def store(self, repo_path, branch):
-        return GitCommitStore(repo_path, branch=branch)
+    def store(self, repo_path, branch, subdir_name):
+        return GitCommitStore(repo_path, branch=branch, subdir=subdir_name)
+
+    def test_uses_subdir(self, repo_path, store, subdir_name, branch):
+        # add a key
+        store.put('foo', b'bar')
+
+        sdir = subdir_name.strip('/')
+
+        fn = 'foo'
+
+        if sdir:
+            fn = sdir + '/' + fn
+
+        repo = Repo(repo_path)
+        commit = repo[repo.refs[b'refs/heads/' + branch]]
+        tree = repo[commit.tree]
+        _, blob_id = tree.lookup_path(repo.__getitem__, fn)
+
+        assert repo[blob_id].data == b'bar'
+
+        # add a second key, resulting in a new commit and two keys available
+        store.put('foo2', b'bar2')
+
+        sdir = subdir_name.strip('/')
+
+        fn = 'foo'
+        if sdir:
+            fn = sdir + '/' + fn
+
+        repo = Repo(repo_path)
+        commit = repo[repo.refs[b'refs/heads/' + branch]]
+        tree = repo[commit.tree]
+        _, blob_id = tree.lookup_path(repo.__getitem__, fn)
+        assert repo[blob_id].data == b'bar'
+
+        fn2 = 'foo2'
+        if sdir:
+            fn2 = sdir + '/' + fn2
+        _, blob_id = tree.lookup_path(repo.__getitem__, fn2)
+        assert repo[blob_id].data == b'bar2'
