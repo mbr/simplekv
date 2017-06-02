6f29f8bd6ce8f9749cd7825498392eaf273089fd
Cornelius Riemenschneider <cornelius.riemenschneider@blue-yonder.com>
2017-06-02 17:55:29 +0200

    Add more config options necessary to connect to minio.

--- tests/bucket_manager.py
+++ tests/bucket_manager.py
@@ -12,12 +12,13 @@ boto = pytest.importorskip('boto')
 @contextmanager
 def boto_bucket(access_key, secret_key, host,
                 connect_func='connect_s3', ordinary_calling_format=False,
-                bucket_name=None):
+                bucket_name=None, port=None, is_secure=True):
     if ordinary_calling_format:
         from boto.s3.connection import OrdinaryCallingFormat
         conn = getattr(boto, connect_func)(access_key, secret_key, host=host,
                                            calling_format=OrdinaryCallingFormat
-                                           ())
+                                           (),
+                                           port=port, is_secure=is_secure)
     else:
         conn = getattr(boto, connect_func)(access_key, secret_key, host=host)
 
@@ -46,7 +47,9 @@ def load_boto_credentials():
     cfg_fn = 'boto_credentials.ini'
 
     parser = ConfigParser({'host': 's3.amazonaws.com',
-                           'ordinary_calling_format': False})
+                           'is_secure': True,
+                           'ordinary_calling_format': False,
+                           'port': None})
     if not parser.read(cfg_fn):
         pytest.skip('file {} not found'.format(cfg_fn))
 
@@ -56,6 +59,8 @@ def load_boto_credentials():
             'secret_key': parser.get(section, 'secret_key'),
             'connect_func': parser.get(section, 'connect_func'),
             'host': parser.get(section, 'host'),
+            'is_secure': parser.getboolean(section, 'is_secure'),
+            'port': parser.getint(section, 'port'),
             'ordinary_calling_format':
                 parser.getboolean(section, 'ordinary_calling_format')
         }
