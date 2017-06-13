760d47b5e692ddc1a1adeaa4de8c4f95c129052d
Cornelius Riemenschneider <cornelius.riemenschneider@blue-yonder.com>
2017-06-13 14:30:56 +0200

    Add tests which test the url-encoding.

--- tests/test_url_encode_keys_decorator.py
+++ tests/test_url_encode_keys_decorator.py
@@ -10,13 +10,22 @@ from basic_store import BasicStore
 
 class TestURLEncodeKeysDecorator(BasicStore):
     @pytest.fixture()
-    def store(self):
-        base_store = DictStore()
+    def base_store(self):
+        return DictStore()
+
+    @pytest.fixture()
+    def store(self, base_store):
         return URLEncodeKeys(base_store)
 
     def test_urlencode(self, store):
         store.put(u'key special:-🍺', b'val1')
         assert store.get(u'key special:-🍺') == b'val1'
+
+    def test_urlencode_base_store(self, store, base_store, value):
+        store.put(u'abc def/key', value)
+        assert u'abc+def%2Fkey' in base_store
+        assert base_store.get(u'abc+def%2Fkey') == value
+        assert store.get(u'abc def/key') == value
 
     # The invalid key is replaced by a valid one after encoding through
     # the decorator...
