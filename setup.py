8a8ae3300bffad0d9cec21a7ccead669c8009b1e
Cornelius Riemenschneider <cornelius.riemenschneider@blue-yonder.com>
2017-11-28 16:41:06 +0100

    Adjust version number.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 setup(name='simplekv',
-      version='0.11.4',
+      version='0.11.5',
       description=('A key-value storage for binary data, support many '
                    'backends.'),
       long_description=read('README.rst'),
