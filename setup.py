0a1a1f5052b7ed5e39e34a61e22f74e34bf636e2
Felix Marczinowski <felix.marczinowski@blue-yonder.com>
2018-07-09 18:40:28 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 setup(name='simplekv',
-      version='0.11.8',
+      version='0.11.9',
       description=('A key-value storage for binary data, support many '
                    'backends.'),
       long_description=read('README.rst'),
