bfa34671123f5a39c6deb6257222982a8dfe029b
Marco Neumann <marco.neumann@blue-yonder.com>
2019-06-12 11:30:12 +0200

    fix travis

--- .travis/start_minio.sh
+++ .travis/start_minio.sh
@@ -3,17 +3,12 @@
 set -e
 set -x
 
-mkdir -p gopath/bin
-export GOPATH=~/gopath
-export PATH=~/gopath/bin:$PATH
-
-go get -u github.com/minio/minio
+wget -O ~/minio https://dl.min.io/server/minio/release/linux-amd64/minio
+chmod +x ~/minio
 
 export MINIO_ACCESS_KEY=minio
 export MINIO_SECRET_KEY=miniostorage
 
-go version
-
 mkdir -p ~/s3
-minio version
-minio server ~/s3 &
+~/minio version
+~/minio server ~/s3 &
