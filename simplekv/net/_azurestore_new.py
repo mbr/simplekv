5e686f8a5c86529dfad133a9d41b3e8254e331b4
Jochen Ott <jochen.ott@blue-yonder.com>
2019-11-08 11:53:38 +0000

    Add support for azure-storage-python 12

---
+++ simplekv/net/_azurestore_new.py
@@ -0,0 +1,238 @@
+"""
+This implements the AzureBlockBlobStore for `azure-storage-blob~=12`
+"""
+import hashlib
+import io
+from contextlib import contextmanager
+
+from .._compat import binary_type
+from .. import KeyValueStore
+
+from ._azurestore_common import (
+    _byte_buffer_md5,
+    _file_md5,
+    lazy_property,
+    LAZY_PROPERTY_ATTR_PREFIX,
+)
+
+
+@contextmanager
+def map_azure_exceptions(key=None, error_codes_pass=()):
+    """Map Azure-specific exceptions to the simplekv-API."""
+    from azure.core.exceptions import AzureError
+
+    try:
+        yield
+    except AzureError as ex:
+        error_code = getattr(ex, "error_code", None)
+        if error_code is not None and error_code in error_codes_pass:
+            return
+        if error_code == "BlobNotFound":
+            raise KeyError(key)
+        raise IOError(str(ex))
+
+
+class AzureBlockBlobStore(KeyValueStore):
+    def __init__(
+        self,
+        conn_string=None,
+        container=None,
+        public=False,
+        create_if_missing=True,
+        max_connections=2,
+        max_block_size=None,
+        max_single_put_size=None,
+        checksum=False,
+        socket_timeout=None,
+    ):
+        """
+        Note that socket_timeout is unused;
+        they only exist for backward compatibility.
+        """
+        self.conn_string = conn_string
+        self.container = container
+        self.public = public
+        self.create_if_missing = create_if_missing
+        self.max_connections = max_connections
+        self.max_block_size = max_block_size
+        self.max_single_put_size = max_single_put_size
+        self.checksum = checksum
+
+    # Using @lazy_property will (re-)create block_blob_service instance needed.
+    # Together with the __getstate__ implementation below, this allows
+    # AzureBlockBlobStore to be pickled, even if
+    # azure.storage.blob.BlockBlobService does not support pickling.
+    @lazy_property
+    def blob_container_client(self):
+        from azure.storage.blob import BlobServiceClient
+
+        kwargs = {}
+        if self.max_single_put_size:
+            kwargs["max_single_put_size"] = self.max_single_put_size
+
+        if self.max_block_size:
+            kwargs["max_block_size"] = self.max_block_size
+
+        service_client = BlobServiceClient.from_connection_string(
+            self.conn_string, **kwargs
+        )
+        container_client = service_client.get_container_client(self.container)
+        if self.create_if_missing:
+            with map_azure_exceptions(error_codes_pass=("ContainerAlreadyExists")):
+                container_client.create_container(
+                    public_access="container" if self.public else None
+                )
+        return container_client
+
+    def _delete(self, key):
+        with map_azure_exceptions(key, error_codes_pass=("BlobNotFound",)):
+            self.blob_container_client.delete_blob(key)
+
+    def _get(self, key):
+        with map_azure_exceptions(key):
+            blob_client = self.blob_container_client.get_blob_client(key)
+            downloader = blob_client.download_blob(max_concurrency=self.max_connections)
+            return downloader.readall()
+
+    def _has_key(self, key):
+        return self.blob_container_client.exists(key)
+
+    def iter_keys(self, prefix=None):
+        blobs = self.blob_container_client.list_blobs(name_starts_with=prefix)
+        return (blob.name for blob in blobs)
+
+    def iter_prefixes(self, delimiter, prefix=u""):
+        return (
+            blob_prefix.name
+            for blob_prefix in self.blob_container_client.walk_blobs(
+                name_starts_with=prefix, delimiter=delimiter
+            )
+        )
+
+    def _open(self, key):
+        with map_azure_exceptions(key):
+            blob_client = self.blob_container_client.get_blob_client(key)
+            return IOInterface(blob_client, self.max_connections)
+
+    def _put(self, key, data):
+        from azure.storage.blob import ContentSettings
+
+        if self.checksum:
+            content_settings = ContentSettings(content_md5=_byte_buffer_md5(data, b64encode=False))
+        else:
+            content_settings = ContentSettings()
+
+        with map_azure_exceptions(key):
+            blob_client = self.blob_container_client.get_blob_client(key)
+
+            blob_client.upload_blob(
+                data,
+                overwrite=True,
+                content_settings=content_settings,
+                max_concurrency=self.max_connections,
+            )
+        return key
+
+    def _put_file(self, key, file):
+        from azure.storage.blob import ContentSettings
+
+        if self.checksum:
+            content_settings = ContentSettings(content_md5=_file_md5(file, b64encode=False))
+        else:
+            content_settings = ContentSettings()
+
+        with map_azure_exceptions(key):
+            blob_client = self.blob_container_client.get_blob_client(key)
+
+            blob_client.upload_blob(
+                file,
+                overwrite=True,
+                content_settings=content_settings,
+                max_concurrency=self.max_connections,
+            )
+        return key
+
+    def _get_file(self, key, file):
+        with map_azure_exceptions(key):
+            blob_client = self.blob_container_client.get_blob_client(key)
+            downloader = blob_client.download_blob(max_concurrency=self.max_connections)
+            downloader.readinto(file)
+
+    def __getstate__(self):
+        # keep all of __dict__, except lazy properties:
+        return {
+            key: value
+            for key, value in self.__dict__.items()
+            if not key.startswith(LAZY_PROPERTY_ATTR_PREFIX)
+        }
+
+
+class IOInterface(io.BufferedIOBase):
+    """
+    Class which provides a file-like interface to selectively read from a blob in the blob store.
+    """
+
+    def __init__(self, blob_client, max_connections):
+        super(IOInterface, self).__init__()
+        self.blob_client = blob_client
+        self.max_connections = max_connections
+
+        blob_props = self.blob_client.get_blob_properties()
+        self.size = blob_props.size
+        self.pos = 0
+
+    def tell(self):
+        """Returns he current offset as int. Always >= 0."""
+        if self.closed:
+            raise ValueError("I/O operation on closed file")
+        return self.pos
+
+    def read(self, size=-1):
+        """Returns 'size' amount of bytes or less if there is no more data.
+        If no size is given all data is returned. size can be >= 0."""
+        if self.closed:
+            raise ValueError("I/O operation on closed file")
+        max_size = max(0, self.size - self.pos)
+        if size < 0:
+            size = max_size
+        if size == 0:
+            return b""
+        downloader = self.blob_client.download_blob(
+            self.pos, size, max_concurrency=self.max_connections
+        )
+        b = downloader.readall()
+        self.pos += len(b)
+        return b
+
+    def seek(self, offset, whence=0):
+        """Move to a new offset either relative or absolute. whence=0 is
+        absolute, whence=1 is relative, whence=2 is relative to the end.
+
+        Any relative or absolute seek operation which would result in a
+        negative position is undefined and that case can be ignored
+        in the implementation.
+
+        Any seek operation which moves the position after the stream
+        should succeed. tell() should report that position and read()
+        should return an empty bytes object."""
+        if self.closed:
+            raise ValueError("I/O operation on closed file")
+        if whence == 0:
+            if offset < 0:
+                raise IOError("seek would move position outside the file")
+            self.pos = offset
+        elif whence == 1:
+            if self.pos + offset < 0:
+                raise IOError("seek would move position outside the file")
+            self.pos += offset
+        elif whence == 2:
+            if self.size + offset < 0:
+                raise IOError("seek would move position outside the file")
+            self.pos = self.size + offset
+        return self.pos
+
+    def seekable(self):
+        return True
+
+    def readable(self):
+        return True
--- tests/test_azure_store.py
+++ tests/test_azure_store.py
@@ -5,6 +5,7 @@ from simplekv.contrib import ExtendedKeyspaceMixin
 from basic_store import BasicStore, OpenSeekTellStore
 from conftest import ExtendedKeyspaceTests
 import pytest
+from base64 import b64encode
 
 pytest.importorskip('azure.storage.blob')
 
@@ -36,18 +37,34 @@ def create_azure_conn_string(credentials):
     return fmt_str.format(account_name, account_key)
 
 
+def _delete_container(conn_string, container):
+    try:
+        # for azure-storage-blob>=12:
+        from azure.storage.blob import BlobServiceClient
+        from azure.core.exceptions import AzureError
+
+        s = BlobServiceClient.from_connection_string(conn_string)
+        try:
+            s.delete_container(container)
+        except AzureError as ex:
+            # ignore the ContainerNotFound error:
+            if ex.error_code != 'ContainerNotFound':
+                raise
+    except ImportError:
+        # for azure-storage-blob<12
+        from azure.storage.blob import BlockBlobService
+        s = BlockBlobService(connection_string=conn_string)
+        s.delete_container(container)
+
+
 class TestAzureStorage(BasicStore, OpenSeekTellStore):
     @pytest.fixture
     def store(self):
-        from azure.storage.blob import BlockBlobService
-
-        container = uuid()
+        container = str(uuid())
         conn_string = create_azure_conn_string(load_azure_credentials())
-        s = BlockBlobService(connection_string=conn_string)
-
         yield AzureBlockBlobStore(conn_string=conn_string, container=container,
                                   public=False)
-        s.delete_container(container)
+        _delete_container(conn_string, container)
 
 
 class TestExtendedKeysAzureStorage(TestAzureStorage, ExtendedKeyspaceTests):
@@ -55,22 +72,17 @@ class TestExtendedKeysAzureStorage(TestAzureStorage, ExtendedKeyspaceTests):
     def store(self):
         class ExtendedKeysStore(ExtendedKeyspaceMixin, AzureBlockBlobStore):
             pass
-        from azure.storage.blob import BlockBlobService
 
-        container = uuid()
+        container = str(uuid())
         conn_string = create_azure_conn_string(load_azure_credentials())
-        s = BlockBlobService(connection_string=conn_string)
-
         yield ExtendedKeysStore(conn_string=conn_string,
                                 container=container, public=False)
-        s.delete_container(container)
+        _delete_container(conn_string, container)
 
 
 def test_azure_setgetstate():
-    from azure.storage.blob import BlockBlobService
-    container = uuid()
+    container = str(uuid())
     conn_string = create_azure_conn_string(load_azure_credentials())
-    s = BlockBlobService(connection_string=conn_string)
     store = AzureBlockBlobStore(conn_string=conn_string, container=container)
     store.put(u'key1', b'value1')
 
@@ -78,7 +90,7 @@ def test_azure_setgetstate():
     store = pickle.loads(buf)
 
     assert store.get(u'key1') == b'value1'
-    s.delete_container(container)
+    _delete_container(conn_string, container)
 
 
 def test_azure_store_attributes():
@@ -112,30 +124,41 @@ class TestAzureExceptionHandling(object):
         assert u"The specified container does not exist." in str(exc.value)
 
     def test_wrong_endpoint(self):
-        from azure.storage.common.retry import ExponentialRetry
-        container = uuid()
+        container = str(uuid())
         conn_string = create_azure_conn_string(load_azure_credentials())
         conn_string += \
             ";BlobEndpoint=https://hopenostorethere.blob.core.windows.net;"
         store = AzureBlockBlobStore(conn_string=conn_string,
                                     container=container,
                                     create_if_missing=False)
-        store.block_blob_service.retry = ExponentialRetry(max_attempts=0).retry
+        if hasattr(store, 'block_blob_service'):
+            from azure.storage.common.retry import ExponentialRetry
+            store.block_blob_service.retry = ExponentialRetry(
+                max_attempts=0
+            ).retry
+        else:
+            store.blob_container_client._config.retry_policy.total_retries = 0
 
         with pytest.raises(IOError) as exc:
             store.put(u"key", b"data")
-        assert u"Failed to establish a new connection" in str(exc.value)
+        assert u"connect" in str(exc.value)
 
     def test_wrong_credentials(self):
-        from azure.storage.common.retry import ExponentialRetry
-        container = uuid()
+        container = str(uuid())
         conn_string = \
             'DefaultEndpointsProtocol=https;AccountName={};AccountKey={}'.\
             format("testaccount", "wrongsecret")
         store = AzureBlockBlobStore(conn_string=conn_string,
                                     container=container,
                                     create_if_missing=False)
-        store.block_blob_service.retry = ExponentialRetry(max_attempts=0).retry
+
+        if hasattr(store, 'block_blob_service'):
+            from azure.storage.common.retry import ExponentialRetry
+            store.block_blob_service.retry = ExponentialRetry(
+                max_attempts=0
+            ).retry
+        else:
+            store.blob_container_client._config.retry_policy.total_retries = 0
 
         with pytest.raises(IOError) as exc:
             store.put(u"key", b"data")
@@ -149,11 +172,8 @@ class TestChecksum(object):
 
     @pytest.fixture
     def store(self):
-        from azure.storage.blob import BlockBlobService
-
-        container = uuid()
+        container = str(uuid())
         conn_string = create_azure_conn_string(load_azure_credentials())
-        s = BlockBlobService(connection_string=conn_string)
 
         yield AzureBlockBlobStore(
             conn_string=conn_string,
@@ -161,13 +181,20 @@ class TestChecksum(object):
             public=False,
             checksum=True,
         )
-        s.delete_container(container)
+        _delete_container(conn_string, container)
 
     def _checksum(self, store):
-        return store.block_blob_service.get_blob_properties(
-            store.container,
-            self.KEY,
-        ).properties.content_settings.content_md5
+        # request the md5 checksum from azure and return the b64 encoded value
+        if hasattr(store, 'block_blob_service'):
+            return store.block_blob_service.get_blob_properties(
+                store.container,
+                self.KEY,
+            ).properties.content_settings.content_md5
+        else:
+            checksum_bytes = store.blob_container_client.get_blob_client(
+                self.KEY
+            ).get_blob_properties().content_settings.content_md5
+            return b64encode(checksum_bytes).decode()
 
     def test_checksum_put(self, store):
         store.put(self.KEY, self.CONTENT)
