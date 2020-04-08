f87e2da2758e22589adf482bfe10128868dc100f
Jochen Ott <Jochen.Ott@jda.com>
2020-04-08 19:09:05 +0200

    bump version number to 0.14.1

--- simplekv/__init__.py
+++ simplekv/__init__.py
@@ -5,7 +5,7 @@ import re
 from io import BytesIO
 from ._compat import key_type
 
-__version__ = '0.14.0'
+__version__ = '0.14.1'
 
 VALID_NON_NUM = r"""\`\!"#$%&'()+,-.<=>?@[]^_{}~"""
 VALID_KEY_REGEXP = "^[%s0-9a-zA-Z]+$" % re.escape(VALID_NON_NUM)
