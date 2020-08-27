42aa9cd23e47c9d08848918bf118bd9cf63e8d1f
Spencer McIntyre <zeroSteiner@gmail.com>
2020-08-26 21:18:36 -0400

    Remove tests for unsupported features

--- tests/test_boto3_store.py
+++ tests/test_boto3_store.py
@@ -6,10 +6,8 @@ import pytest
 
 boto3 = pytest.importorskip('boto3')
 from simplekv.net.boto3store import Boto3Store
-from simplekv._compat import BytesIO
 
 from basic_store import BasicStore
-from url_store import UrlStore
 from bucket_manager import boto_credentials, boto3_bucket
 from conftest import ExtendedKeyspaceTests
 from simplekv.contrib import ExtendedKeyspaceMixin
@@ -27,15 +25,7 @@ def bucket(credentials):
         yield bucket
 
 
-class TestBoto3Storage(BasicStore, UrlStore):
-    @pytest.fixture(params=[True, False])
-    def reduced_redundancy(self, request):
-        return request.param
-
-    @pytest.fixture
-    def storage_class(self, reduced_redundancy):
-        return 'REDUCED_REDUNDANCY' if reduced_redundancy else 'STANDARD'
-
+class TestBoto3Storage(BasicStore):
     @pytest.fixture(params=['', '/test-prefix'])
     def prefix(self, request):
         return request.param
@@ -54,29 +44,6 @@ class TestBoto3Storage(BasicStore, UrlStore):
     def test_key_error_on_nonexistant_get_filename(self, store, key, tmp_path):
         with pytest.raises(KeyError):
             store.get_file(key, os.path.join(str(tmp_path), 'a'))
-
-    def test_storage_class_put(
-        self, store, prefix, key, value, storage_class, bucket
-    ):
-        store.put(key, value)
-
-        keyname = prefix + key
-
-        if storage_class != 'STANDARD':
-            pytest.xfail('boto does not support checking the storage class?')
-
-        assert bucket.Object(keyname).storage_class == storage_class
-
-    def test_storage_class_putfile(
-        self, store, prefix, key, value, storage_class, bucket
-    ):
-        store.put_file(key, BytesIO(value))
-
-        keyname = prefix + key
-
-        if storage_class != 'STANDARD':
-            pytest.xfail('boto does not support checking the storage class?')
-        assert bucket.Object(keyname).storage_class == storage_class
 
 
 class TestExtendedKeyspaceBoto3Store(TestBoto3Storage, ExtendedKeyspaceTests):
