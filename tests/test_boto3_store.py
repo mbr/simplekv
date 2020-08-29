fa26c795afddbbd9f0eed0af856a3b6c04836c71
Spencer McIntyre <zeroSteiner@gmail.com>
2020-08-29 15:39:10 -0400

    Support the reduced redundancy storage class and metadata for boto3

--- tests/test_boto3_store.py
+++ tests/test_boto3_store.py
@@ -6,6 +6,7 @@ import pytest
 
 boto3 = pytest.importorskip('boto3')
 from simplekv.net.boto3store import Boto3Store
+from simplekv._compat import BytesIO
 
 from basic_store import BasicStore
 from url_store import UrlStore
@@ -27,13 +28,21 @@ def bucket(credentials):
 
 
 class TestBoto3Storage(BasicStore, UrlStore):
+    @pytest.fixture(params=[True, False])
+    def reduced_redundancy(self, request):
+        return request.param
+
+    @pytest.fixture
+    def storage_class(self, reduced_redundancy):
+        return 'REDUCED_REDUNDANCY' if reduced_redundancy else None
+
     @pytest.fixture(params=['', '/test-prefix'])
     def prefix(self, request):
         return request.param
 
     @pytest.fixture
-    def store(self, bucket, prefix):
-        return Boto3Store(bucket, prefix)
+    def store(self, bucket, prefix, reduced_redundancy):
+        return Boto3Store(bucket, prefix, reduced_redundancy=reduced_redundancy)
 
     # Disable max key length test as it leads to problems with minio
     test_max_key_length = None
@@ -46,10 +55,25 @@ class TestBoto3Storage(BasicStore, UrlStore):
         with pytest.raises(KeyError):
             store.get_file(key, os.path.join(str(tmp_path), 'a'))
 
+    def test_storage_class_put(
+        self, store, prefix, key, value, storage_class, bucket
+    ):
+        store.put(key, value)
+        obj = bucket.Object(prefix.lstrip('/') + key)
+        assert obj.storage_class == storage_class
+
+    def test_storage_class_putfile(
+        self, store, prefix, key, value, storage_class, bucket
+    ):
+        store.put_file(key, BytesIO(value))
+        obj = bucket.Object(prefix.lstrip('/') + key)
+        assert obj.storage_class == storage_class
+
 
 class TestExtendedKeyspaceBoto3Store(TestBoto3Storage, ExtendedKeyspaceTests):
     @pytest.fixture
-    def store(self, bucket, prefix):
+    def store(self, bucket, prefix, reduced_redundancy):
         class ExtendedKeyspaceStore(ExtendedKeyspaceMixin, Boto3Store):
             pass
-        return ExtendedKeyspaceStore(bucket, prefix)
+        return ExtendedKeyspaceStore(bucket, prefix,
+                                     reduced_redundancy=reduced_redundancy)
